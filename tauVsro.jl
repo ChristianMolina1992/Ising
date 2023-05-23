013a9497ceb310a3be557fd283885a1efc15d34b
ChristianMolina1992 <96879220+ChristianMolina1992@users.noreply.github.com>
2023-05-23 14:23:30 -0300

    Create tauVsro.jl

---
+++ tauVsro.jl
@@ -0,0 +1,47 @@
+using JLD
+using LatticeModels
+using BioStatPhys
+using DelimitedFiles
+using Plots 
+
+function tauVStime(;L,nlong,ntimes,mlen)
+    IS = Ising(SQLattice_periodic,L,L)
+    conf = load("/Users/christian/SQconf_Tc_L$(L).jld","IS.σ")
+    IS.σ = conf
+    set_energy_mag!(IS)
+    set_temperature!(IS,Ising_SQ_critical_temperature)
+    epsilon=zeros(Float64,nlong)  
+    times=zeros(Float64,ntimes)
+    fail = 0
+    
+    i = 1
+    pos = zeros(L^2, 2)
+    for x = 1:L, y = 1:L
+        pos[i, 1] = x
+        pos[i, 2] = y
+        i += 1
+    end
+     binning = distance_binning(pos, 1.0, rmin=0.0)
+    
+    for i ∈ eachindex(epsilon)   
+        try
+            _,M = Wolff!(IS,steps=mlen,save_interval=1)
+            s=reshape(IS.σ,L^2)           
+            r,C = space_correlation(binning, reshape(IS.σ,L^2), connected=true,normalized=true)
+            epsilon[i] = correlation_length_r0(r,C)
+            
+            _,M = Metropolis!(IS,steps=mlen,save_interval=1)
+            M = transpose(M)
+            C=BioStatPhys.time_correlation_tw_direct(M,connected=true,i0=1,Xmean=zeros(size(M)),normalized=true)
+            times[i]=correlation_time_spectral(C,1)
+    catch
+            fail += 1
+        end
+    end
+    
+    if fail>0 @warn "Failed $(fail)" 
+    end
+    writedlm("tamaño_ro_$(L)_$(nlong)_$(mlen).txt",epsilon)
+    writedlm("tamaño_tau_$(L)_$(ntimes)_$(mlen).txt",times)
+    return epsilon,times
+end
