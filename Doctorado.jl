91970b46a6e050fc652c97183770e3e04be212b0
ChristianMolina1992 <96879220+ChristianMolina1992@users.noreply.github.com>
2024-01-17 12:47:12 -0300

    Update Doctorado.jl

--- Doctorado.jl
+++ Doctorado.jl
@@ -2,7 +2,7 @@ using JLD
 using LatticeModels
 using DelimitedFiles
 
-# Para crear las configuraciones de Wolff
+# Para crear las configuraciones de Wolff. Con este programa puedo ir guardando las configuraciones como así tambien ir calculando las magnetizaciones por cada configuración e ir guardándolas
 
 L = 50
 
@@ -10,7 +10,7 @@ n = 5000
 nterm = 5000
 ndata = n + nterm
 Magnetizacion = []
-# Tc=Ising_SQ_critical_temperature
+Tc=Ising_SQ_critical_temperature
 
 function configuracion(;T,L,nterm,n)
     for i in 1:ndata
@@ -19,11 +19,43 @@ function configuracion(;T,L,nterm,n)
         Wolff!(IS, steps=ndata)
         
         # Guardar la configuración en un archivo de texto con formato personalizado
-        ruta = "/home/cmolina/Documentos/Julia/SQconfig_L$(L)_Temp$(T)_ndata$(i).txt"
+        ruta = "/home/cmolina/Documentos/Julia/Configuraciones/SQconfig_L$(L)_Temp$(T)_ndata$(i).txt"
         writedlm(ruta, IS.σ)
-        mag=IS.M
-        push!(Magnetizacion,mag)
+        #mag=IS.M
+        push!(Magnetizacion,M)
+        writedlm("/home/cmolina/Documentos/Julia/Configuraciones/magnetizacion$(L).txt",M)
     end
 end
 
-#Acordate de cmo calcular la susceptibilidad y poner la temperatura critica, y ademas normalizar la temperatura.
+
+
+##Como me di cuenta que el programa de arriba no me arrojaba las magnetizaciones por spin, entonces realicé otro programa para que me la devolviera y finalmente grafíqué haciendo using Plots 
+#plot((1:10000),magnetizaciones,label=false,ylabel="Magnetización",xlabel="Tiempo")
+
+function magnetization_ising2d(s)
+    m, n = size(s)
+    return sum(s)/(m*n)
+end
+
+ruta = "/home/cmolina/Documentos/Julia/"
+nombre_en_comun = "SQconfig_L50_Temp2.269185314213022_ndata"
+
+cant_archivos = 1:10000
+magnetizaciones = []
+
+for i in cant_archivos
+    nombre_archivo = joinpath(ruta, "$nombre_en_comun$i.txt")
+    
+    try
+        # Leer el archivo
+        s = readdlm(nombre_archivo)
+        
+        # Aplicar la función y almacenar el resultado
+        magnetizacion = abs(magnetization_ising2d(s))
+        
+        push!(magnetizaciones, magnetizacion)
+        
+    catch e
+    end
+end
+
