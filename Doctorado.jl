52db4f48dc7624f843f55d526d01a6b6fb1ffcbd
ChristianMolina1992 <96879220+ChristianMolina1992@users.noreply.github.com>
2024-01-23 14:58:24 -0300

    Update Doctorado.jl

--- Doctorado.jl
+++ Doctorado.jl
@@ -59,3 +59,42 @@ for i in cant_archivos
     end
 end
 
+
+
+
+
+using JLD
+using LatticeModels
+using DelimitedFiles
+using Statistics
+
+# Para crear las configuraciones de Wolff
+
+
+function configuracion(;T,L,nterm,n)
+        IS = Ising(SQLattice_periodic, L, L, ordered=true)
+        set_temperature!(IS, T)
+        Wolff!(IS, steps=nterm)
+        e,m = Wolff!(IS,steps=n,save_interval=10)
+        # Guardar la configuración en un archivo de texto con formato personalizado
+        ruta = "/home/cmolina/Documentos/Julia/Config1.0/SQconfig_L$(L)_Temp$(T)_ndata$(n).txt"
+        writedlm(ruta, IS.σ)
+
+        mag=mean(abs.(m))
+        sus=L*L*var(m)
+        return mag,sus
+ end
+
+
+
+L = 20
+Tc=Ising_SQ_critical_temperature
+
+magnetizacion_20 = Float64[]
+susceptibility_20 = Float64[]
+for T=0.2*Tc:0.05:1.4*Tc
+    mag,sus = configuracion(T=T,L=L,nterm=5000,n=10000)
+    push!(magnetizacion_20,mag)
+    push!(susceptibility_20,sus)
+end
+
